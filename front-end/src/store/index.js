b44924a3b5cacdf865cce5c30bb7033c1fa41935
jaeho <jaho901@naver.com>
2022-01-11 13:35:15 +0900

    s

---
+++ front-end/src/store/index.js
@@ -0,0 +1,16 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+import createPersistedState from 'vuex-persistedstate'
+
+
+export default Vuex.Store({
+  plugin: [createPersistedState(),],
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
