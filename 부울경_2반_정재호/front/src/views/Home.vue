3ad82b0f408009a35ed36135e7452341852fe7af
정재호 <jaho901@naver.com>
2022-01-17 19:58:12 +0900

    planning

---
+++ 부울경_2반_정재호/front/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <p>homepage</p>
+    <router-link class="btn" :to="{ name: 'Login' }">Login</router-link>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  setup() {
+  }
+}
+</script>
