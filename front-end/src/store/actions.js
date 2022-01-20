7485b39a4a67df16e1002b90155c864c3e923076
정재호 <jaho901@naver.com>
2022-01-20 14:04:36 +0900

    sign up/in is changed

--- front-end/src/store/actions.js
+++ front-end/src/store/actions.js
@@ -4,6 +4,7 @@ export function signUp ({state}, payload) {
   console.log('signUp', state, payload)
   const url = '/user/signup'
   let body = payload
+  console.log($axios.post(url, body))
   return $axios.post(url, body)
 }
 
@@ -11,6 +12,7 @@ export function signIn ({state}, payload) {
   console.log('signIn', state, payload)
   const url = '/user/signin'
   let body = payload
+  console.log($axios.post(url, body))
   return $axios.post(url, body)
 }
 
