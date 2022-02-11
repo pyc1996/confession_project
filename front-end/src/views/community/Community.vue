8fce8932135eb70f5c51caf459700b3593442465
lewns2 <lewns2002@naver.com>
2022-02-11 11:28:13 +0900

    front2

--- front-end/src/views/community/Community.vue
+++ front-end/src/views/community/Community.vue
@@ -1,13 +1,39 @@
 <template>
-  
+  <div>
+    <!-- <button @click="goToCreateCommunity">게시글 생성</button> -->
+    <community-view :userInfo="state.userInfo"></community-view>
+  </div>
 </template>
 
 <script>
+import { reactive, computed, onMounted } from "vue";
+import { useStore } from "vuex";
+import { useRouter } from 'vue-router'
+import CommunityView from './components/CommunityView.vue';
+
 export default {
+    name: "Confession",
+    components: { CommunityView },
+
+    setup() {
+      const store = useStore();
+      const router = useRouter()
+      const state = reactive({
+        userInfo: computed(()=> store.getters['root/userInfo']),
+      })
+
+      onMounted(() => {
+        store.dispatch("root/communityGetCommunityList")
+      })
+
+      // const goToCreateCommunity = function () {
+      //   router.push({ name : "CommunityCreate" })
+      // }
+
+      return { state, onMounted }
+    }
 
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
