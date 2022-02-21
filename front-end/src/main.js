c380e12bbdd7de187532ff7a09cdddb498579a1a
정재호 <jaho901@naver.com>
2022-02-21 18:38:24 +0900

    checked

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -5,17 +5,6 @@ import store from "./common/lib/store";
 import VueAxios from "./common/lib/axios";
 import axios from "./common/lib/axios";
 
-
-// global styles
-// import "./assets/main.css";
-
-// const router = new useRouter({
-//   routes,
-//   scrollBehavior (to, from, savedPosition) {
-//     return { x: 0, y: 0}
-//   }
-// })
-
 const app = createApp({
   render: () => h(App),
 });
